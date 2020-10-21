1af404a168d043806cbf6cfe7820d393977d06d9
Chelsea McGovern <cgmcgovern1@gmail.com>
2020-10-21 17:23:26 -0400

    Created skeleton of connect method

--- simple_social_network.py
+++ simple_social_network.py
@@ -7,5 +7,16 @@ class Person:
         name(str): the person's name.
         connections (set of Person): other perople in the social network who know this person.
     """
-    
+    def __init__(self, name):
+        """ Initialize a new Person object"""
+        self.name = name
+        self.connections = set()
+        
+    def connect (self, person2):
+        """ Connect with person2.
+        
+        Args:
+            person2(Person): the other person to connect to.
+        """
+        
 
